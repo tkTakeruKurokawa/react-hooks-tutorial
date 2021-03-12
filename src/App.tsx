61bd37ecf4f90eb27b58f93d7759658439101b5d
tkTakeruKurokawa <kurokawa.takeru830@icloud.com>
2021-03-12 19:00:28 +0900

    complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,99 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { BookToRead } from "./BookToRead";
+import { BookDescription } from "./BookDescription";
+import BookRow from "./BookRow";
+import Modal from "react-modal";
+import BookSearchDialog from "./BookSearchDialog";
 
-const dummyBooks: BookToRead[] = [
-  {
-    id: 1,
-    title: "はじめてのReact",
-    authors: "ダミー",
-    memo: ""
+Modal.setAppElement("#root");
+
+const customStyles = {
+  overlay: {
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
   },
-  {
-    id: 2,
-    title: "React Hooks入門",
-    authors: "ダミー",
-    memo: ""
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    padding: 0,
+    transform: "translate(-50%, -50%)",
   },
-  {
-    id: 3,
-    title: "実践Reactアプリケーション開発",
-    authors: "ダミー",
-    memo: ""
-  }
-];
+};
+
+const APP_KEY = "react-hooks-tutorial";
 
 const App = () => {
+  const [books, setBooks] = useState([] as BookToRead[]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  useEffect(() => {
+    const storedBooks = localStorage.getItem(APP_KEY);
+    if (storedBooks) {
+      setBooks(JSON.parse(storedBooks));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(APP_KEY, JSON.stringify(books));
+  }, [books]);
+
+  const handleBookMemoChange = (id: number, memo: string) => {
+    const newBooks = books.map((b) => {
+      return b.id === id ? { ...b, memo: memo } : b;
+    });
+    setBooks(newBooks);
+  };
+
+  const handleBookDelete = (id: number) => {
+    const newBooks = books.filter((b) => b.id !== id);
+    setBooks(newBooks);
+  };
+
+  const handleAddClick = () => {
+    setModalIsOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setModalIsOpen(false);
+  };
+
+  const handleBookAdd = (book: BookDescription) => {
+    const newBook: BookToRead = { ...book, id: Date.now(), memo: "" };
+    const newBooks = [...books, newBook];
+    setBooks(newBooks);
+    setModalIsOpen(false);
+  };
+
+  const bookRows = books.map((b) => {
+    return (
+      <BookRow
+        book={b}
+        key={b.id}
+        onMemoChange={(id, memo) => handleBookMemoChange(id, memo)}
+        onDelete={(id) => handleBookDelete(id)}
+      />
+    );
+  });
+
   return (
     <div className="App">
       <section className="nav">
         <h1>読みたい本リスト</h1>
-        <div className="button-like">本を追加</div>
+        <div className="button-like" onClick={handleAddClick}>
+          本を追加
+        </div>
       </section>
-      <section className="main">
-        <h1>チュートリアルを始めましょう</h1>
-      </section>
+      <section className="main">{bookRows}</section>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={handleModalClose}
+        style={customStyles}
+      >
+        <BookSearchDialog maxResults={20} onBookAdd={(b) => handleBookAdd(b)} />
+      </Modal>
     </div>
   );
 };
